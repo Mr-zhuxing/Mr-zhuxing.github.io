c97e595d97afcf4c7fd6192e4d468062e3157520
zhu_xing <1172254048@qq.com>
2021-04-21 22:10:36 +0800

    iit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const jsx = (
+  <section>
+    <h1>慢慢</h1>
+    <h1>全栈</h1>
+    <a href="www.baidu.com">kkb</a>
+  </section>
+)
+
+console.log(jsx)
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  jsx,
   document.getElementById('root')
 );
 
